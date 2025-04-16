d8c46abd51660ef243384127b3a098b67d96826c
Tejasve <tejasve.g@cloudare.in>
2025-04-16 14:10:23 +0530

    Cloudare YT Video Added

--- src/Components/Home/CourseRated.jsx
+++ src/Components/Home/CourseRated.jsx
@@ -49,7 +49,7 @@ const CourseRated = () => {
             <iframe
               width="100%"
               height="100%"
-              src="https://www.youtube.com/embed/VigFI4TuwzI"
+              src="https://www.youtube.com/embed/OeTmV6rk0Ig"
               title="Learnbay Success Stories"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
